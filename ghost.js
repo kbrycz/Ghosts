0bdb4d928f71df4f7a4c7789714895c684ddde36
kbrycz <kbrycz@umich.edu>
2020-04-22 13:25:32 -0400

    Fixed bug

--- ghost.js
+++ ghost.js
@@ -186,4 +186,4 @@ io.on('connection', (socket) => {
 
 // -----------------LISTEN ON PORT 80-----------------
 
-http.listen(3000, () => console.log('listening on port 80'))
+http.listen(8081, () => console.log('listening on port 8081'))
