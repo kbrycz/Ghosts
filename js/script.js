ff800659a5ec1cc6dc0dacb29c0c02b63fb960dd
kbrycz <kbrycz@umich.edu>
2020-04-27 14:14:58 -0400

    Fixed bug

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ data: {
 // -----------------------------------------------------------APPLICATION OPENING FUNCTIONS---------------------------------------------------------------------
 created: function () {
     // Current server that we are connecting to
-    socket = io('ws://localhost:8081');
+    socket = io('ws://ghostsonline.me:8081');
     if (window.location.search !== '') {
         let room = window.location.search.toString();
         room = room.toUpperCase();
